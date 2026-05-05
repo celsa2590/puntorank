59bbb6f146f98b1e6560946afd2f4327760699f0
Celsa <ccssantana25@gmail.com>
2026-05-05 13:28:34 -0400

    Cambiando backend 2

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:8080",
-        "http://127.0.0.1:8080"
+        "http://127.0.0.1:8080",
         "https://puntorank-frontend.onrender.com"
     ],
     allow_credentials=True,
