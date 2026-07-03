3c3eb8c06f305cdcdb1f6b89445f7cf4adae86be
Celsa <ccssantana25@gmail.com>
2026-07-03 10:51:54 -0400

    agregar origins puntorank.cl al main

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,8 @@ app.add_middleware(
         "http://localhost:8080",
         "http://127.0.0.1:8080",
         "https://puntorank-frontend.onrender.com",
-        "https://puntorank-backend.onrender.com"
+        "https://puntorank-backend.onrender.com",
+        "https://puntorank.cl"
     ],
     allow_credentials=True,
     allow_methods=["*"],
